0f1320ea59eaffcdc197bc2993e657874ec0773c
FernandoIzidio <luizfernando1010200@gmail.com>
2024-03-21 17:26:04 -0300

    create Controller AdminController

---
+++ app/controllers/AdminController.php
@@ -0,0 +1,16 @@
+<?php
+
+use app\config\RootProject;
+use app\controllers\BaseController;
+
+require_once("../app/config/config.php");
+require_once RootProject::getRootPath()->controllers . "/BaseController.php";
+
+class AdminController extends BaseController {
+
+    public function getAdmin(){
+        
+
+    }
+
+}
