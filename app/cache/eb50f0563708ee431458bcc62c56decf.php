2049898dfb32b8c8539ca0a5f6a6ae7e45238251
FernandoIzidio <luizfernando1010200@gmail.com>
2024-03-29 22:31:19 -0300

    modify loggout view

--- app/cache/eb50f0563708ee431458bcc62c56decf.php
+++ app/cache/eb50f0563708ee431458bcc62c56decf.php
@@ -1,5 +1,5 @@
 <?php $__env->startSection("links"); ?>
-    <li><a href="/dashboard/loggout">Loggout</a></li>
+    <li><a href="/loggout">Loggout</a></li>
 <?php $__env->stopSection(); ?>
 
 <?php echo $__env->make("global.base", \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/Rating-System/app/views/Dashboard/profile.blade.php ENDPATH**/ ?>
